eca0bfd7b9d12ec868a9063884964feeab8bd647
GennySeKro <614732599@qq.com>
2024-02-22 18:14:16 +0800

    148

--- src/HOT148.java
+++ src/HOT148.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class HOT148 {
     /*
@@ -9,7 +8,28 @@ public class HOT148 {
      */
 
     public ListNode sortList(ListNode head) {
-        
+        List<Integer> res = new ArrayList<>();
+        ListNode p = head;
+        if (head == null){
+            return null;
+        }
+        while (p != null){
+            res.add(p.val);
+            p = p.next;
+        }
+        int[] value = new int[res.size()];
+        for (int i = 0;i < res.size();i++){
+            value[i] = res.get(i);
+        }
+        Arrays.sort(value);
+        p = head;
+        int i = 0;
+        while (p != null){
+            p.val = value[i];
+            i++;
+            p = p.next;
+        }
+        return head;
     }
 
 }
