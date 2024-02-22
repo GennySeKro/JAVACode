4a412c93da9191fd91cdf5bfbc521c2ed478e559
GennySeKro <614732599@qq.com>
2024-02-22 20:47:06 +0800

    77

--- src/HOT77.java
+++ src/HOT77.java
@@ -12,18 +12,18 @@ public class HOT77 {
     LinkedList<Integer> path = new LinkedList<>();
     List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> combine(int n, int k) {
-        backtracking(n,k,path,res,1);
+        backtracking(n,k,1);
         return res;
     }
 
-    private void backtracking(int n, int k, LinkedList<Integer> path, List<List<Integer>> res, int index) {
+    private void backtracking(int n, int k, int index) {
         if(path.size() == k){
             res.add(new ArrayList<>(path));
             return;
         }
         for(int i = index;i <= n - (k - path.size()) + 1; i ++){
             path.add(i);
-            backtracking(n,k,path,res,i + 1);
+            backtracking(n,k,i + 1);
             path.removeLast();
         }
     }
