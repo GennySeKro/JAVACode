6bf30bbbd232d0419c3bfa1bc5f39311ee5ff45b
GennySekro <614732599@qq.com>
2024-02-27 23:14:32 +0800

    2024-2-27

---
+++ src/LCR/LCR061.java
@@ -0,0 +1,12 @@
+package LCR;
+import java.util.*;
+
+public class LCR061 {
+    /*
+    查找和最小的K对数字
+     */
+
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+
+    }
+}
